ef91682a292feb010a605b71bf0fe55c85f6cf4d
Birzhan <https://github.com/birzhaaan/Vue_practice.git>
2026-07-11 13:42:45 +0500

    fix: add Vercel domain to CORS_ALLOWED_ORIGINS

--- esg-report/backend/backend/settings.py
+++ esg-report/backend/backend/settings.py
@@ -125,9 +125,11 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://esg-non-financial-reporting-platfor.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'https://esg-non-financial-reporting-platform-production.up.railway.app',
+    'https://esg-non-financial-reporting-platfor.vercel.app'
 ]
