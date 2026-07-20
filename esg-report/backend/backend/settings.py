b4d3eae2300a0d909af8381cee2a4dc3b8fbcfcd
Zhantoresh <z_orazymbetov@kbtu.kz>
2026-07-20 13:23:50 +0500

    fix: allow all CORS origins

--- esg-report/backend/backend/settings.py
+++ esg-report/backend/backend/settings.py
@@ -123,14 +123,11 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://esg-non-financial-reporting-platform-jwaf8l7ri.vercel.app',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'https://esg-non-financial-reporting-platform-production.up.railway.app',
     'https://esg-non-financial-reporting-platform-jwaf8l7ri.vercel.app',
+    'https://esg-non-financial-reporting-platform-oibjir5mf.vercel.app',
 ]
